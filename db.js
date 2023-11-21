02cd9da19658637dff404b8ebe20a7dccdd78108
Omer <feedodeedo@gmail.com>
2023-11-21 01:16:13 -0500

    modified db.js

--- db.js
+++ db.js
@@ -4,15 +4,19 @@ const pg = require("pg");
 const { DB_NAME, DB_USER, DB_HOST, DB_PASSWORD, DB_PORT } = require("./config");
 
 const db = new pg.Client({
-  user: DB_USER, // "postgres",
-  host: DB_HOST, // "localhost",
+  user: DB_USER,
+  host: DB_HOST,
   database: DB_NAME,
-  password: DB_PASSWORD, //"postgres",
-  port: DB_PORT, // 5432,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  // ssl: {
+  //   rejectUnauthorized: false, // process.env.NODE_ENV === "production" ? false : true,
+  // },
 });
+
+process.env.NODE_ENV === "production"
+  ? (db.ssl.rejectUnauthorized = false)
+  : null;
 
 db.connect();
 
